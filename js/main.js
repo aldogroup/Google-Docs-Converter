6431541ec60149a25dee8eced904989f32a51871
Chris Berthe <chrisberthe@gmail.com>
2013-10-29 14:52:53 -0400

    Export JS to an external file and link to minified version

---
+++ js/main.js
@@ -0,0 +1,65 @@
+;(function($) {
+	$('input').focus();
+
+	var request;
+
+	$('form').submit(function(event) {
+
+		event.preventDefault();
+
+	    if (request) {
+	        request.abort();
+	    }
+
+	    var $form = $(this),
+	    	$loading = $('#loading'),
+	    	$response = $('#json-response');
+	    
+	    var $inputs = $form.find("input, select, button, textarea");
+	    
+	    var serializedData = $form.serialize();
+
+	    request = $.ajax({
+	        url: "converter.json.php",
+	        type: "post",
+	        data: serializedData,
+	        beforeSend: function(xhr) {
+	        	$loading.fadeIn(100);
+	        }
+	    });
+
+	    request.done(function (response, textStatus, jqXHR) {
+	        // log a message to the console
+	        console.log("Hooray, it worked!");
+
+	        $form.hide();
+
+	        $response.slideDown('fast', function() {
+	        	$(this).html(response);
+	        });
+
+	        $('.restart').show().on('click', function(e) {
+				e.preventDefault();
+
+				if ( ! $(this).is(':hidden')) {
+					$form.show().find('input').val('').focus();
+					$response.hide();
+					$(this).hide();
+				}
+			});
+	    });
+
+	    request.fail(function (jqXHR, textStatus, errorThrown) {
+	        console.error(
+	            "The following error occured: "+
+	            textStatus, errorThrown
+	        );
+
+	        alert('There was a problem');
+	    });
+
+	    request.always(function () {
+	        $loading.fadeOut(100);
+	    });
+	});
+})(jQuery);
