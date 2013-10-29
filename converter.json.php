f2bc480d2277202ea42edfeaf98246a0c7ea6364
Chris Berthe <chrisberthe@gmail.com>
2013-10-29 14:43:26 -0400

    Initial commit

---
+++ converter.json.php
@@ -0,0 +1,69 @@
+<?php
+ 
+/**
+ * Converts the CSV output from Google Docs to an array
+ * we can work with.
+ *
+ * @string $file Google docs URL
+ * @string $delimiter The seperator
+ *
+ * @return $arr CSV converted array
+ */
+function csvToArray($file, $delimiter) { 
+  if (($handle = fopen($file, 'r')) !== FALSE) { 
+    $i = 0; 
+    while (($lineArray = fgetcsv($handle, 4000, $delimiter, '"')) !== FALSE) {
+      for ($j = 0; $j < count($lineArray); $j++) { 
+        $arr[$i][$j] = $lineArray[$j]; 
+      } 
+      $i++; 
+    } 
+    fclose($handle); 
+  } 
+  return $arr; 
+} 
+
+/**
+ * Encodes an array to JSON format - in this case
+ * the Google Docs URL we pass as a parameter
+ *
+ * @string $feed Google Docs URL
+ *
+ * @return $newArray Encoded JSON
+ */
+function encodeArraytoJSON($feed = '') {
+  $keys = array();
+  $newArray = array();
+
+  $data = csvToArray($feed, ',');
+  $count = count($data) - 1;
+  $labels = array_shift($data);  
+   
+  foreach ($labels as $label) {
+    $keys[] = $label;
+  }
+
+  $keys[] = 'id';
+   
+  for ($i = 0; $i < $count; $i++) {
+    $data[$i][] = $i;
+  }
+   
+  for ($j = 0; $j < $count; $j++) {
+    $d = array_combine($keys, $data[$j]);
+    $newArray[$j] = $d;
+  }
+
+  echo json_encode($newArray);
+}
+
+if ( $_POST['key'] == '' ) {
+    echo 'Empty value';
+    return;
+}
+
+// Format the Google Docs link before converting
+$docsUrl = 'https://docs.google.com/spreadsheet/pub?key=' . $_POST['key'] . '&single=true&gid=0&output=csv';
+
+// Bombs away!
+encodeArraytoJSON($docsUrl);
---
+++ index.php
@@ -0,0 +1,107 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Google Docs Converter</title>
+	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+	<link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css">
+	<link rel="stylesheet" href="css/styles.css">
+</head>
+<body>
+	<div id="wrapper">
+		<h3 class="title">Google Docs Converter</h3>
+		<div id="wrapper-content">
+			Input the Google Docs <strong>page key</strong> to convert to JSON.
+
+			<textarea class="form-control" id="json-response" rows="12"></textarea>
+
+			<form role="form" class="form">
+				<div class="form-group">
+					<input type="text" class="form-control" id="key" name="key" placeholder="Enter value for ?key=">
+				</div>
+				<button type="submit" class="btn btn-success">Submit</button>
+				<div id="loading" class="spin-1"></div>
+			</form>
+
+			<a href="#" class="restart">
+				<small>Enter a new value</small>
+			</a>
+		</div>
+	</div>
+
+	<script>
+		;(function($) {
+			$('input').focus();
+
+			var request;
+
+			$('form').submit(function(event){
+			    if (request) {
+			        request.abort();
+			    }
+			    // setup some local variables
+			    var $form = $(this),
+			    	$loading = $('#loading'),
+			    	$response = $('#json-response');
+			    
+			    var $inputs = $form.find("input, select, button, textarea");
+			    
+			    var serializedData = $form.serialize();
+
+			    // fire off the request to /form.php
+			    request = $.ajax({
+			        url: "converter.json.php",
+			        type: "post",
+			        data: serializedData,
+			        beforeSend: function(xhr) {
+			        	$loading.fadeIn(100);
+			        }
+			    });
+
+			    // callback handler that will be called on success
+			    request.done(function (response, textStatus, jqXHR) {
+			        // log a message to the console
+			        console.log("Hooray, it worked!");
+
+			        $form.hide();
+
+			        $response.fadeIn('fast', function() {
+			        	$(this).html(response);
+			        });
+
+			        // Click link to go back to enter new value
+			        $('.restart').show().on('click', function(e) {
+						e.preventDefault();
+
+						if ( ! $(this).is(':hidden')) {
+							$form.show().find('input').val('').focus();
+							$response.hide();
+							$(this).hide();
+						}
+					});
+			    });
+
+			    request.fail(function (jqXHR, textStatus, errorThrown) {
+			        console.error(
+			            "The following error occured: "+
+			            textStatus, errorThrown
+			        );
+
+			        alert('There was a problem');
+			    });
+
+			    // callback handler that will be called regardless
+			    // if the request failed or succeeded
+			    request.always(function () {
+			        // reenable the inputs
+			        $inputs.prop("disabled", false);
+			        $loading.fadeOut(100);
+			    });
+
+			    // prevent default posting of form
+			    event.preventDefault();
+			});
+		})(jQuery);
+	</script>
+</body>
+</html>
