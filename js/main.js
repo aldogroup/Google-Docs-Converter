fd2727bf4e31ca9d87e3baae8723e386b97b6f8e
Chris Berthe <chrisberthe@gmail.com>
2013-11-22 11:16:27 -0500

    Modify indentation to 2 space

--- js/main.js
+++ js/main.js
@@ -14,50 +14,51 @@
 	    var $form = $(this),
 	    	$loading = $('#loading'),
 	    	$response = $('#json-response');
-	    
+
 	    var $inputs = $form.find("input, select, button, textarea");
-	    
+
 	    var serializedData = $form.serialize();
 
 	    request = $.ajax({
-	        url: "converter.json.php",
-	        type: "post",
-	        data: serializedData,
-	        beforeSend: function(xhr) {
-	        	$loading.fadeIn(100);
-	        }
+        url: "converter.json.php",
+        type: "post",
+        data: serializedData,
+        beforeSend: function(xhr) {
+        	$loading.fadeIn(100);
+        }
 	    });
 
 	    request.done(function (response, textStatus, jqXHR) {
-	    	
-	        $form.hide();
 
-	        $response.slideDown('fast', function() {
-	        	$(this).html(response).select();
-	        });
+        $form.hide();
 
-	        $('.restart').show().on('click', function(e) {
-				e.preventDefault();
+        $response.slideDown('fast', function() {
+        	$(this).html(response).select();
+        });
 
-				if ( ! $(this).is(':hidden')) {
-					$form.show().find('input').val('').focus();
-					$response.hide();
-					$(this).hide();
-				}
-			});
+        $('.restart').show().on('click', function(e) {
+					e.preventDefault();
+
+					if ( ! $(this).is(':hidden')) {
+						$form.show().find('input').val('').focus();
+						$response.hide();
+						$(this).hide();
+					}
+				});
 	    });
 
 	    request.fail(function (jqXHR, textStatus, errorThrown) {
-	        console.error(
-	            "The following error occured: "+
-	            textStatus, errorThrown
-	        );
+        console.error(
+            "The following error occured: "+
+            textStatus, errorThrown
+        );
 
-	        alert('There was a problem');
+        alert('There was a problem');
 	    });
 
 	    request.always(function () {
-	        $loading.fadeOut(100);
+	    	$loading.fadeOut(100);
 	    });
 	});
+
 })(jQuery);
